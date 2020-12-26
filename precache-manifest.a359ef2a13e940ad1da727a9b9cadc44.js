59683f15b29ef5d5cbb960448d35db4f65d61891
czhu1217 <cindyzhu0830@gmail.com>
2020-12-25 23:26:03 -0500

    Updates

--- precache-manifest.a359ef2a13e940ad1da727a9b9cadc44.js
+++ precache-manifest.a359ef2a13e940ad1da727a9b9cadc44.js
@@ -1,102 +1,102 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c3927a88a1a142eeff9524e4c2b4b12c",
-    "url": "/index.html"
+    "revision": "4e1f4b940d23834a5ad72d93fb9f25c1",
+    "url": "/my-app/index.html"
   },
   {
     "revision": "cf16dfefd887b155f8f9",
-    "url": "/static/css/main.81b2e154.chunk.css"
+    "url": "/my-app/static/css/main.81b2e154.chunk.css"
   },
   {
     "revision": "d39b333e770d71811484",
-    "url": "/static/js/2.6a7fce49.chunk.js"
+    "url": "/my-app/static/js/2.6a7fce49.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/static/js/2.6a7fce49.chunk.js.LICENSE.txt"
+    "url": "/my-app/static/js/2.6a7fce49.chunk.js.LICENSE.txt"
   },
   {
     "revision": "cf16dfefd887b155f8f9",
-    "url": "/static/js/main.0124b8c1.chunk.js"
+    "url": "/my-app/static/js/main.0124b8c1.chunk.js"
   },
   {
-    "revision": "d6bf746fd6f3ab16e1ca",
-    "url": "/static/js/runtime-main.c0e5beb8.js"
+    "revision": "b806ff52d54f6357d588",
+    "url": "/my-app/static/js/runtime-main.d26e7d40.js"
   },
   {
     "revision": "a13bca2f5e9fc89703607ad4bd7f8b4c",
-    "url": "/static/media/berlin1.a13bca2f.jpeg"
+    "url": "/my-app/static/media/berlin1.a13bca2f.jpeg"
   },
   {
     "revision": "93ec6f51f6fe0939cfb7ecff8785c131",
-    "url": "/static/media/bvg1.93ec6f51.png"
+    "url": "/my-app/static/media/bvg1.93ec6f51.png"
   },
   {
     "revision": "50715a14d2615e6ea1a958f3c3049be3",
-    "url": "/static/media/bvg2.50715a14.jpeg"
+    "url": "/my-app/static/media/bvg2.50715a14.jpeg"
   },
   {
     "revision": "071565220bbb5e16551c5cfbafea865f",
-    "url": "/static/media/bvg3.07156522.jpeg"
+    "url": "/my-app/static/media/bvg3.07156522.jpeg"
   },
   {
     "revision": "23a86a63c5a2e8a7ab73a82f78cdd86a",
-    "url": "/static/media/coda.23a86a63.png"
+    "url": "/my-app/static/media/coda.23a86a63.png"
   },
   {
     "revision": "54835c1bba540ffe87ded7e8a7964db3",
-    "url": "/static/media/eeart.54835c1b.png"
+    "url": "/my-app/static/media/eeart.54835c1b.png"
   },
   {
     "revision": "32c35e149c0a6e8eec0a2fff4de90f2f",
-    "url": "/static/media/finders.32c35e14.png"
+    "url": "/my-app/static/media/finders.32c35e14.png"
   },
   {
     "revision": "e08e8ff819c627c217c6532d1d38ee5b",
-    "url": "/static/media/grip2.e08e8ff8.png"
+    "url": "/my-app/static/media/grip2.e08e8ff8.png"
   },
   {
     "revision": "74e5092f1625fa912da4143c39c6ae38",
-    "url": "/static/media/gripindoor2.74e5092f.png"
+    "url": "/my-app/static/media/gripindoor2.74e5092f.png"
   },
   {
     "revision": "e5f666505878b105bb8b0e84fa75b1a0",
-    "url": "/static/media/keepers.e5f66650.png"
+    "url": "/my-app/static/media/keepers.e5f66650.png"
   },
   {
     "revision": "542ec3e717d0ea76a47280f70bf6d330",
-    "url": "/static/media/melewi.542ec3e7.png"
+    "url": "/my-app/static/media/melewi.542ec3e7.png"
   },
   {
     "revision": "89224401795c4ba300ef60a2c6a8a83a",
-    "url": "/static/media/podcast.89224401.png"
+    "url": "/my-app/static/media/podcast.89224401.png"
   },
   {
     "revision": "52ee068c7ffb0a60bcd401db6b8699eb",
-    "url": "/static/media/quora1.52ee068c.png"
+    "url": "/my-app/static/media/quora1.52ee068c.png"
   },
   {
     "revision": "96ce9c20f33c3eedb2010026edda2d43",
-    "url": "/static/media/quora2.96ce9c20.png"
+    "url": "/my-app/static/media/quora2.96ce9c20.png"
   },
   {
     "revision": "08f3b41b29d17803b361fff48a4ef086",
-    "url": "/static/media/quora3.08f3b41b.png"
+    "url": "/my-app/static/media/quora3.08f3b41b.png"
   },
   {
     "revision": "8f2f40a4be7dc8d5b5a2e857fe09af4a",
-    "url": "/static/media/results.8f2f40a4.png"
+    "url": "/my-app/static/media/results.8f2f40a4.png"
   },
   {
     "revision": "38ddf213242ee35274b3eb76da65f211",
-    "url": "/static/media/rv.38ddf213.png"
+    "url": "/my-app/static/media/rv.38ddf213.png"
   },
   {
     "revision": "cba575e59bbdffb781e0bd668d031beb",
-    "url": "/static/media/ski.cba575e5.png"
+    "url": "/my-app/static/media/ski.cba575e5.png"
   },
   {
     "revision": "c12514fdfa4edd3529b8399d667af204",
-    "url": "/static/media/word.c12514fd.png"
+    "url": "/my-app/static/media/word.c12514fd.png"
   }
 ]);
